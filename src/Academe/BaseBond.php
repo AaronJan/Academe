2e6712f69b502373cc9d23601693ef2816f41b30
AaronJan <aaronjan@qq.com>
2016-09-21 10:32:29 +0800

    add abstract class BaseBond to ease up many-to-many relation definition

---
+++ src/Academe/BaseBond.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Academe;
+
+use Academe\Relation\Contracts\Bond as BondContract;
+
+abstract class BaseBond implements BondContract
+{
+    /**
+     * @return string|null
+     */
+    public function connectionName()
+    {
+        return null;
+    }
+
+    /**
+     * @return array
+     */
+    public function relations()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function castRules()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function customs()
+    {
+        return [];
+    }
+
+    /**
+     * @return null|string
+     */
+    public function mapperClass()
+    {
+        return null;
+    }
+
+    /**
+     * @return string
+     */
+    public function pivotField()
+    {
+        return 'pivot';
+    }
+}
