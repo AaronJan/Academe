499f1267fad1587121055ae1e355bf35c313daed
AaronJan <aaronjan@qq.com>
2017-03-29 14:14:15 +0800

    Model now extends ArrayObject for array casting

--- src/Academe/Model.php
+++ src/Academe/Model.php
@@ -2,13 +2,8 @@
 
 namespace Academe;
 
-class Model implements \ArrayAccess, \JsonSerializable
+class Model extends \ArrayObject implements \JsonSerializable
 {
-    /**
-     * @var array
-     */
-    protected $attributes;
-
     /**
      * Model constructor.
      *
@@ -16,7 +11,7 @@ class Model implements \ArrayAccess, \JsonSerializable
      */
     public function __construct(array $attribtes = [])
     {
-        $this->attributes = $attribtes;
+        parent::__construct($attribtes, \ArrayObject::ARRAY_AS_PROPS);
     }
 
     /**
@@ -26,68 +21,6 @@ class Model implements \ArrayAccess, \JsonSerializable
     public function newInstance(array $attributes)
     {
         return new static($attributes);
-    }
-
-    /**
-     * @param $name
-     * @return mixed
-     */
-    public function __get($name)
-    {
-        return $this->offsetGet($name);
-    }
-
-    /**
-     * @param $name
-     * @param $value
-     */
-    public function __set($name, $value)
-    {
-        $this->offsetSet($name, $value);
-    }
-
-    /**
-     * @param $name
-     * @return bool
-     */
-    public function __isset($name)
-    {
-        return isset($this->attributes[$name]);
-    }
-
-    /**
-     * @param mixed $offset
-     * @return bool
-     */
-    public function offsetExists($offset)
-    {
-        return isset($this->attributes[$offset]);
-    }
-
-    /**
-     * @param mixed $offset
-     * @return mixed
-     */
-    public function offsetGet($offset)
-    {
-        return $this->attributes[$offset] ?? null;
-    }
-
-    /**
-     * @param mixed $offset
-     * @param mixed $value
-     */
-    public function offsetSet($offset, $value)
-    {
-        $this->attributes[$offset] = $value;
-    }
-
-    /**
-     * @param mixed $offset
-     */
-    public function offsetUnset($offset)
-    {
-        unset($this->attributes[$offset]);
     }
 
     /**
@@ -103,7 +36,7 @@ class Model implements \ArrayAccess, \JsonSerializable
      */
     function toArray()
     {
-        return $this->attributes;
+        return (array) $this;
     }
 
 }
