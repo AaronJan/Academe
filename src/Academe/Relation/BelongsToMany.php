3a6fb2ac310a9bda1acbf50c76aacb0fc70a7fb2
AaronJan <aaronjan@qq.com>
2018-04-16 20:18:31 +0800

    `BelongsToMany` can specify foreign keys for host and guest Blueprint other than primary keys

--- src/Academe/Relation/BelongsToMany.php
+++ src/Academe/Relation/BelongsToMany.php
@@ -20,6 +20,16 @@ class BelongsToMany implements Relation
     protected $isHost;
 
     /**
+     * @var null|string
+     */
+    protected $hostForeignKey;
+
+    /**
+     * @var null|string
+     */
+    protected $guestForeignKey;
+
+    /**
      * @var null|\Academe\Contracts\Connection\Condition|\Academe\ConditionGroup
      */
     protected $pivotCondition;
@@ -32,24 +42,28 @@ class BelongsToMany implements Relation
     /**
      * BelongsToMany constructor.
      *
-     * @param      $bondClass
+     * @param string $bondClass
      * @param bool $isHost
+     * @param null|string $hostForeignKey
+     * @param null|string $guestForeignKey
      */
-    public function __construct($bondClass, $isHost)
+    public function __construct($bondClass, $isHost, $hostForeignKey = null, $guestForeignKey = null)
     {
         $this->bondClass = $bondClass;
-        $this->isHost    = $isHost;
+        $this->isHost = $isHost;
+        $this->hostForeignKey = $hostForeignKey;
+        $this->guestForeignKey = $guestForeignKey;
     }
 
     /**
      * @param \Academe\Contracts\Mapper\Mapper $hostMapper
-     * @param string                           $relationName
-     * @param Academe                          $academe
+     * @param string $relationName
+     * @param Academe $academe
      * @return \Academe\Relation\Contracts\RelationHandler
      */
     public function makeHandler(Mapper $hostMapper, $relationName, Academe $academe)
     {
-        return new BelongsToManyRelationHandler($this, $relationName);
+        return new BelongsToManyRelationHandler($this, $relationName, $this->hostForeignKey, $this->guestForeignKey);
     }
 
     /**
